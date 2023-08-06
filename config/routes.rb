53bf67278b784ae94116cdec8aa3e96bb580fd8a
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-08-06 03:54:09 +0000

    header

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,30 @@
 Rails.application.routes.draw do
 
-  #ユーザー用
+  #ユーザーログイン
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-    #管理者用
+
+  #管理者ログイン
   devise_for :admin, controllers: {
     sessions: "admin/sessions"
   }
 
+  #ユーザー側アクション
   scope module: :public do
+    root to: "roads#index"
     get 'homes/about'
-    resources :users, only: [:show, :edit] do
+    resources :users, only: [:index, :edit] do
       get :unscribe, on: :collection
       patch :withdrawal, on: :collection
     end
     resources :roads, except: [:destroy]
   end
 
-
+  #管理者側アクション
   namespace :admin do
+    root to: "roads#index"
     resources :users, only: [:index, :show, :edit]
     resources :roads, only: [:index, :show, :edit]
   end
