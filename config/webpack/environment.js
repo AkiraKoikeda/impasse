6f2f7882a44f3903ac13c2bc776760e52bc9b25b
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-08-09 05:38:10 +0000

    No Push

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,11 +3,12 @@ const { environment } = require('@rails/webpacker')
 module.exports = environment
 
 const webpack = require('webpack')
-enviroment.plugin.prepend(
+environment.plugins.prepend(
   'Provide',
-  new webpack.PovidePlugin({
+  new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
 )
+
