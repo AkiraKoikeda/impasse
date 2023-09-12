ae3dba2470e8bddd7fc1e4aa8bf70bffccee7c15
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-09-12 14:38:48 +0000

    comment_fix

--- app/controllers/public/road_comments_controller.rb
+++ app/controllers/public/road_comments_controller.rb
@@ -2,9 +2,9 @@
 
 class Public::RoadCommentsController < ApplicationController
   def create
-    road = Road.find(params[:road_id])
+    @road = Road.find(params[:road_id])
     comment = current_user.road_comments.new(road_comment_params)
-    comment.road_id = road.id
+    comment.road_id = @road.id
     unless comment.save
       flash[:alert] = "正しい情報を入力してください"
     end
