0e5961dd453b1a6951afd4ed17f7cfa7080ed36a
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-10-25 09:35:23 +0000

    admin_fix

--- spec/system/admin_login_spec.rb
+++ spec/system/admin_login_spec.rb
@@ -4,6 +4,12 @@ require 'rails_helper'
 
 describe 'トップ画面のテスト' do
   before do
-    visit admen_root_path
+    visit admin_root_path
+  end
+  context '表示の確認' do
+    it 'URLが正しい' do
+      expect(current_path).to eq '/admin'
+    end
+    it ''
   end
 end
